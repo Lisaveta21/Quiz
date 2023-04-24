158984d0445af65df1bba5e76fdb9db89ee8d571
Lisa <Liza@desktop>
2023-04-24 19:06:04 +0300

    Add project files.

---
+++ Йгшя/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Йгшя
+{
+    class Quiz
+    {
+        private List<Question> questions;
+        private int score;
+
+        public Quiz()
+        {
+            questions = new List<Question>();
+            score = 0;
+        }
+
+        public void AddQuestion(Question question)
+        {
+            questions.Add(question);
+        }
+
+        public void Start()
+        {
+            score = 0;
+
+            Console.WriteLine("Начинаем новую викторину!");
+
+            foreach (Question question in questions)
+            {
+                Console.WriteLine(question.Text);
+                Console.WriteLine("Варианты ответов:");
+                for (int i = 0; i < question.Options.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {question.Options[i]}");
+                }
+
+                Console.Write("Введите номер ответа: ");
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == question.CorrectOption)
+                {
+                    Console.WriteLine("Правильно!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Неправильно! Правильный ответ: {question.CorrectOption}");
+                }
+            }
+
+            Console.WriteLine($"Вы набрали {score} баллов!");
+        }
+    }
+
+    class Question
+    {
+        public string Text;
+        public List<string> Options;
+        public int CorrectOption;
+
+        public Question(string text, List<string> options, int correctOption)
+        {
+            Text = text;
+            Options = options;
+            CorrectOption = correctOption;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quiz quiz = new Quiz();
+
+            // добавляем вопросы
+            quiz.AddQuestion(new Question("Сколько будет 2 + 2?", new List<string> { "3", "4", "5" }, 2));
+            quiz.AddQuestion(new Question("Какой год был основан Рим?", new List<string> { "753 г. до н.э.", "44 г. до н.э.", "476 г. н.э." }, 1));
+
+            // запускаем викторину
+            quiz.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Йгшя/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Йгшя;
+using static System.Console;
+
+namespace Quiz
+
+{
+
+    static void Register(string login, string password)
+    {
+       
+        System.IO.File.WriteAllText(login + ".txt", password);
+    }
+
+    static void Main(string[] args)
+    {
+        bool loggedIn = false; 
+
+        while (!loggedIn) 
+        {
+            Console.WriteLine("Введите логин:");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Введите пароль:");
+            string password = Console.ReadLine();
+
+          
+            
+                Console.WriteLine("Пользователь с таким логином и паролем не найден. Хотите зарегистрироваться? (да/нет)");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "да") 
+                {
+                    Register(login, password); 
+                    Console.WriteLine("Регистрация прошла успешно. Теперь вы можете войти в систему.");
+                }
+                else 
+                {
+                    Console.WriteLine("Выход из программы.");
+                    return; // выходим из программы
+                }
+            
+        }
+
+        Quiz quiz = new Quiz();
+
+        // добавляем вопросы
+        quiz.AddQuestion(new Question("Сколько будет 2 + 2?", new List<string> { "3", "4", "5" }, 2));
+        quiz.AddQuestion(new Question("Какой год был основан Рим?", new List<string> { "753 г. до н.э.", "44 г. до н.э.", "476 г. н.э." }, 1));
+
+        // запускаем викторину
+        quiz.Start();
+
+        Console.ReadLine();
+    }
+
+   
+
+    
+}
+
